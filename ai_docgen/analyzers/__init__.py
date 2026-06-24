d5bb05aa55242fdd9afd6befb40a62dc7a55b31f
zizegak916-glitch <zizegak916@users.noreply.github.com>
2026-06-24 12:57:16 +0000

    deploy: GitHub Pages static site

--- ai_docgen/analyzers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseAnalyzer
-from .chrome_ext import ChromeExtensionAnalyzer
-from .android_access import AndroidAccessibilityAnalyzer
-from .ios_app import IOSAppAnalyzer
-
-__all__ = ['BaseAnalyzer', 'ChromeExtensionAnalyzer', 'AndroidAccessibilityAnalyzer', 'IOSAppAnalyzer']
--- ai_docgen/generators/__init__.py
+++
@@ -1,4 +0,0 @@
-from .markdown import MarkdownGenerator
-from .html import HTMLGenerator
-
-__all__ = ['MarkdownGenerator', 'HTMLGenerator']
